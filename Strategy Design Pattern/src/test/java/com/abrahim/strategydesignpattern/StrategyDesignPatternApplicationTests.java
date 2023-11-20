756b27d048cf00386015797097f5be8e476368de
abrahimcse <abrahimcse@gmail.com>
2023-11-20 17:01:32 +0600

    Project Initial

---
+++ Strategy Design Pattern/src/test/java/com/abrahim/strategydesignpattern/StrategyDesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abrahim.strategydesignpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrategyDesignPatternApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
